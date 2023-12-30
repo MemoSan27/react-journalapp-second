429e3dfdb49e31aa70b248dbfc6b563e77bb079e
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2023-12-30 12:33:00 -0700

    login and register page ui created

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,14 @@
+import { Route } from "react-router-dom"
+import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTheme"
 
 
 export const JournalApp = () => {
   return (
     <>
-        <h1> Hola mundo </h1>
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
     </>
   )
 }
