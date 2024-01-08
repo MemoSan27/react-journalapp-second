e23aba8d48b10f9ca126d7f5cc66c7e275b24b83
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-08 11:33:19 -0700

    initializing redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,30 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+
+      <NothingSelectedView /> 
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8, transition: 'all 300ms' },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
+      
+    </JournalLayout>
   )
 }
